82def4de313b54bee0cb6fdf0aca53787d530338
AndreyKonash <108748273+AndreyKonash@users.noreply.github.com>
2023-03-08 07:22:14 +0000

    Deploying to gh-pages from @ AndreyKonash/goit-js-hw-09@8f9933531aed1e7153ece56c494b8437c0962326 🚀

---
+++ 01-color-switcher.29360ec2.js
@@ -0,0 +1,2 @@
+const t={startBtn:document.querySelector("[data-start]"),stopBtn:document.querySelector("[data-stop]")};let n=null;t.startBtn.addEventListener("click",(function(){t.startBtn.disabled=!0,n=setInterval((function(){document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3)})),t.stopBtn.addEventListener("click",(function(){t.startBtn.disabled=!1,clearInterval(n)}));
+//# sourceMappingURL=01-color-switcher.29360ec2.js.map
--- 01-color-switcher.dfe6cdb4.js
+++
@@ -1,2 +0,0 @@
-refs={startBtn:document.querySelector("[data-start]"),stopBtn:document.querySelector("[data-stop]")};let t=null;refs.startBtn.addEventListener("click",(function(){refs.startBtn.disabled=!0,t=setInterval((function(){document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3)})),refs.stopBtn.addEventListener("click",(function(){refs.startBtn.disabled=!1,clearInterval(t)}));
-//# sourceMappingURL=01-color-switcher.dfe6cdb4.js.map
